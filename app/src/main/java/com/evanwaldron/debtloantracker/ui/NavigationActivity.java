7b7051ef6895718ab9bc4273dcfdd7f87e091d97
Evan <evanrwaldron@gmail.com>
2014-09-26 13:53:44 -0400

    Removed PlaceholderFragment (no longer needed), onSectionAttached (no longer ever called), and added in some comments to non inherited methods for NavigationActivity.

--- app/src/main/java/com/evanwaldron/debtloantracker/ui/NavigationActivity.java
+++ app/src/main/java/com/evanwaldron/debtloantracker/ui/NavigationActivity.java
@@ -35,6 +35,9 @@ public class NavigationActivity extends Activity
      */
     private CharSequence mTitle;
 
+    /**
+     * Used to configure action bar based on active fragment. For use in {@link #restoreActionBar()}
+     */
     private ActionBarConfigurer mCurActionBarConfig = null;
 
     @Override
@@ -52,6 +55,9 @@ public class NavigationActivity extends Activity
                 (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
+    /**
+     * Replaces the active fragment with Person List Fragment
+     */
     private void showPersonListFragment(){
         PersonListFragment fragment = new PersonListFragment();
         mCurActionBarConfig = fragment;
@@ -60,6 +66,9 @@ public class NavigationActivity extends Activity
                 .commit();
     }
 
+    /**
+     * Replaces the active fragment with History Fragment
+     */
     private void showHistoryFragment(){
         HistoryFragment fragment = new HistoryFragment();
         mCurActionBarConfig = fragment;
@@ -68,6 +77,10 @@ public class NavigationActivity extends Activity
                 .commit();
     }
 
+    /**
+     * Returns open/closed state of navigation drawer
+     * @return true if open, false if closed
+     */
     boolean isDrawerOpen(){
         return mNavigationDrawerFragment.isDrawerOpen();
     }
@@ -88,17 +101,9 @@ public class NavigationActivity extends Activity
         }
     }
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section_list);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section_history);
-                break;
-        }
-    }
-
+    /**
+     * Configures the action bar based on current active fragment, or defaults if there is no active fragment (should never happen)
+     */
     public void restoreActionBar() {
         if(mCurActionBarConfig == null) {
             restoreDefaultActionBar();
@@ -107,6 +112,9 @@ public class NavigationActivity extends Activity
         }
     }
 
+    /**
+     * Restores the default action bar for the app
+     */
     private void restoreDefaultActionBar(){
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
@@ -149,45 +157,4 @@ public class NavigationActivity extends Activity
         }
         super.onBackPressed();
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_navigation, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((NavigationActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
